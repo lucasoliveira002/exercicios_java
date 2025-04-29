e0ea7a8f7828bc67a04fc27f4f830a05088c4e20
Lucas Oliveira <135052326+lucasoliveira002@users.noreply.github.com>
2025-04-29 16:04:16 -0300

    Add files via upload

--- atividades_classe/ContaBancaria.java
+++ atividades_classe/ContaBancaria.java
@@ -1,5 +1,41 @@
 package atividades;
 
 public class ContaBancaria {
+	private String titular;
+	private Double saldo;
+	private Integer numero;
+	
+	public ContaBancaria(String t, Double s, Integer n ) {
+		this.titular= t;
+		this.saldo = s;
+		this.numero = n;
+	}
 
+	public ContaBancaria(String t, Integer n) {
+		this.titular= t;
+		this.saldo = 0.0;
+		this.numero = n;
+	}
+	
+	//gets
+		public String getTitular() {
+			return titular;
+		}
+		public Double getSaldo() {
+			return saldo;
+		}
+		public Integer getNumero() {
+			return numero;
+		}
+		
+		//sets
+		public void setTitular(String titular) {
+			this.titular = titular;
+		}
+		public void setSaldo(Double saldo) {
+			this.saldo = saldo;
+		}
+		public void setNumero(Integer numero) {
+			this.numero = numero;
+		}
 }
--- atividades_classe/Produto.java
+++ atividades_classe/Produto.java
@@ -1,5 +1,46 @@
 package atividades;
 
 public class Produto {
+	
+	private String nome;
+	private Double preco;
+	private Integer estoque;
+	
+	//construtores
+	public Produto(String n, Double p, Integer e ) {
+		this.nome= n;
+		this.preco = p;
+		this.estoque = e;
+	}
+	
+	public Produto(String n) {
+		this.nome = n;
+		this.preco = 0.0;
+		this.estoque = 0;
+	}
+	
+	
+	//gets
+	public String getNome() {
+		return nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public Integer getEstoque() {
+		return estoque;
+	}
+	
+	//sets
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+	
 
 }
